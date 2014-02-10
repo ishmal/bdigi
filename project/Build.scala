3ed50544049d123a93fcdafac3c1a99ada767823
ishmal <ishmalius@gmail.com>
2014-02-10 13:56:55 -0600

    Add sbt-assembly plugin

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,12 @@
 import sbt._
 import sbt.Keys._
-
+import sbtassembly.Plugin._ 
+import AssemblyKeys._
 
 object MyBuild extends Build {
 
 
-    override lazy val settings = super.settings ++ Seq(
+    lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     
         organization := "org.bdigi",
         version := "1.6",
@@ -15,23 +16,27 @@ object MyBuild extends Build {
     
     lazy val root = Project(
         id = "bdigi",
-        base = file(".") 
+        base = file("."),
+        settings = buildSettings
     ) aggregate (core, fx, andy)
 
     lazy val core = Project(
         id = "core",
-        base = file("core")
+        base = file("core"),
+        settings = buildSettings
     )
 
     lazy val fx = Project(
         id = "fx",
-        base = file("fx")
+        base = file("fx"),
+        settings = buildSettings ++ assemblySettings
     ) dependsOn core
 
 
     lazy val andy = Project(
         id = "andy",
-        base = file("andy")
+        base = file("andy"),
+        settings = buildSettings
     ) dependsOn core
 
 
