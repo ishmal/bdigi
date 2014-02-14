41c464f673b0e5d8454cd89b42c9067c92c7ae22
ishmal <ishmalius@gmail.com>
2014-02-14 14:28:44 -0600

    Got 1200packet working

--- core/src/main/scala/org/bdigi/mode/Packet.scala
+++ core/src/main/scala/org/bdigi/mode/Packet.scala
@@ -29,34 +29,52 @@ package org.bdigi.mode
 import org.bdigi._
 
 
-case class PacketAddr(call: String,  ssid:Int)
-{
 
-    lazy val encoded : Array[Int] =
-        {
-        val add = Array.tabulate(7)(i=>
-             {
-             if (i < call.size)
-                 ((call(i).toInt) << 1)
-             else if (i==6)
-                 (0x60 | (ssid << 1))
-             else
-                 0x40   // shifted space
-             })
-        add
-        }
 
-    override def toString =
-        {
-        if (ssid >= 0) { call + "-" + ssid } else call
-        }
-  
-}
 
-/*
+/**
+ * CRC-CCITT-16 calculator, that handles both big and little-endian byte
+ * streams
+ */
 class CrcCCITT
 {
-	private val crcTab = Array(
+    
+	private val crcTable = Array(
+        0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+        0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
+        0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+        0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
+        0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
+        0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+        0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
+        0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
+        0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+        0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
+        0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
+        0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+        0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
+        0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
+        0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+        0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
+        0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
+        0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+        0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
+        0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
+        0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+        0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+        0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
+        0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+        0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
+        0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
+        0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+        0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
+        0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
+        0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+        0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
+        0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0
+        )
+
+	private val crcTabLE = Array(
         0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
         0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7,
         0x1081, 0x0108, 0x3393, 0x221a, 0x56a5, 0x472c, 0x75b7, 0x643e,
@@ -93,70 +111,6 @@ class CrcCCITT
 
 	private var crc = 0xffff
 	
-	def update(byte8: Int) =
-	    crc = ((crc >> 8) ^ crcTab((crc ^ byte8) & 0xff)) & 0xffff
-	    
-	def value =
-	    crc
-	        
-    def reset = 
-        crc = 0xffff
-}
-*/
-
-
-/**
- * This appears to be the correct algorithm for this task
- */
-class CrcCCITT
-{
-	private val crcTable = Array(
-        0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5,
-        0x60c6, 0x70e7, 0x8108, 0x9129, 0xa14a, 0xb16b,
-        0xc18c, 0xd1ad, 0xe1ce, 0xf1ef, 0x1231, 0x0210,
-        0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
-        0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c,
-        0xf3ff, 0xe3de, 0x2462, 0x3443, 0x0420, 0x1401,
-        0x64e6, 0x74c7, 0x44a4, 0x5485, 0xa56a, 0xb54b,
-        0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
-        0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6,
-        0x5695, 0x46b4, 0xb75b, 0xa77a, 0x9719, 0x8738,
-        0xf7df, 0xe7fe, 0xd79d, 0xc7bc, 0x48c4, 0x58e5,
-        0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
-        0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969,
-        0xa90a, 0xb92b, 0x5af5, 0x4ad4, 0x7ab7, 0x6a96,
-        0x1a71, 0x0a50, 0x3a33, 0x2a12, 0xdbfd, 0xcbdc,
-        0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
-        0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03,
-        0x0c60, 0x1c41, 0xedae, 0xfd8f, 0xcdec, 0xddcd,
-        0xad2a, 0xbd0b, 0x8d68, 0x9d49, 0x7e97, 0x6eb6,
-        0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
-        0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a,
-        0x9f59, 0x8f78, 0x9188, 0x81a9, 0xb1ca, 0xa1eb,
-        0xd10c, 0xc12d, 0xf14e, 0xe16f, 0x1080, 0x00a1,
-        0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
-        0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c,
-        0xe37f, 0xf35e, 0x02b1, 0x1290, 0x22f3, 0x32d2,
-        0x4235, 0x5214, 0x6277, 0x7256, 0xb5ea, 0xa5cb,
-        0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
-        0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447,
-        0x5424, 0x4405, 0xa7db, 0xb7fa, 0x8799, 0x97b8,
-        0xe75f, 0xf77e, 0xc71d, 0xd73c, 0x26d3, 0x36f2,
-        0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
-        0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9,
-        0xb98a, 0xa9ab, 0x5844, 0x4865, 0x7806, 0x6827,
-        0x18c0, 0x08e1, 0x3882, 0x28a3, 0xcb7d, 0xdb5c,
-        0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
-        0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0,
-        0x2ab3, 0x3a92, 0xfd2e, 0xed0f, 0xdd6c, 0xcd4d,
-        0xbdaa, 0xad8b, 0x9de8, 0x8dc9, 0x7c26, 0x6c07,
-        0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
-        0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba,
-        0x8fd9, 0x9ff8, 0x6e17, 0x7e36, 0x4e55, 0x5e74,
-        0x2e93, 0x3eb2, 0x0ed1, 0x1ef0
-        )
-	private var crc = 0xffff
-	
 	def update(c: Int) =
 	    {
 	    val j = (c ^ (crc >> 8)) & 0xff
@@ -166,8 +120,40 @@ class CrcCCITT
 	def value =
 	    (crc ^ 0) & 0xffff
 	        
+	def updateLE(byte8: Int) =
+	    crc = ((crc >> 8) ^ crcTabLE((crc ^ byte8) & 0xff)) & 0xffff
+	    
+	def valueLE =
+	    crc
+
     def reset = 
         crc = 0xffff
+}
+
+
+
+case class PacketAddr(call: String,  ssid:Int)
+{
+
+    lazy val encoded : Array[Int] =
+        {
+        val add = Array.tabulate(7)(i=>
+             {
+             if (i < call.size)
+                 ((call(i).toInt) << 1)
+             else if (i==6)
+                 (0x60 | (ssid << 1))
+             else
+                 0x40   // shifted space
+             })
+        add
+        }
+
+    override def toString =
+        {
+        if (ssid >= 0) { call + "-" + ssid } else call
+        }
+  
 }
 
 
@@ -177,7 +163,7 @@ case class Packet(
     val rpts  : Seq[PacketAddr], 
     val ctrl  : Int, 
     val pid   : Int,
-    val info  : Array[Byte]
+    val info  : Array[Int]
     )
 {
 
@@ -205,8 +191,25 @@ case class Packet(
         
     override def toString : String =
         {
-        val infos = new String(info)
-        "%s -> %s : %s".format(src.toString, dest.toString, infos) 
+        var buf = new StringBuilder
+        buf.append(src.toString).append("=>").append(dest.toString)
+        for (r <- rpts)
+            {
+            buf.append(":").append(r.toString)
+            }
+        buf.append(" [").append(pid.toString).append("]: ")
+        if (pid != 0)
+            {
+            val infos = new String(info.map(_.toByte))
+            buf.append(infos)
+            }
+        else
+            {
+            for (v <- info)
+                buf.append(",").append(v.toString)
+            }
+            
+        buf.toString
         } 
 }
 
@@ -270,13 +273,8 @@ object Packet
     private def getAddr(arr: Array[Int], offset:Int) : PacketAddr =
         {
         var buf = new StringBuilder
-        for (i <- offset until offset + 7)
-            buf.append( (arr(i) >> 1).toChar)
-        val str = buf.toString
-        //println("str:" + buf)
-        val pair = str.split(" ")
-        val call = if (pair.size > 0) pair(0) else ""
-        val ssid = if (pair.size > 1) getInt(pair(1)) else -1
+        var call = new String(arr.slice(offset, offset+6).map(_.toByte & 127)).trim
+        val ssid = arr(offset+6) & 127
         new PacketAddr(call, ssid)
         }
 
@@ -289,16 +287,12 @@ object Packet
         val src  = getAddr(data, pos)
         pos += 7
         val rpts = scala.collection.mutable.ListBuffer[PacketAddr]()
-        if ((data(pos - 1) & 1) == 1)
+        while (rpts.size <= 8 && ((data(pos - 1) & 128) != 0) )
             {
             rpts.append(getAddr(data, pos))
             pos += 7
             }
-        if ((data(pos - 1) & 1) == 1)
-            {
-            rpts.append(getAddr(data, pos))
-            pos += 7
-            }
+
         val ctrl = data(pos)
         pos += 1
         
@@ -307,9 +301,7 @@ object Packet
         val pid = if (typ == IFRAME) data(pos) else 0
         if (typ == IFRAME) pos += 1
         
-        val info = Array.tabulate(data.size - pos - 2)
-            { i => data(i + pos).toByte }
-            
+        val info = data.drop(pos)
         
         val pack = new Packet(dest, src, rpts.toSeq, 0, 0, info)
         pack
@@ -325,10 +317,12 @@ object Packet
 
 /**
  * Mode for AX-25 packet communications.
+ *
+ * Note:  apparently 4800s/s seems to be necessary for this to work on 1200baud
  *  
  * @see http://www.tapr.org/pub_ax25.html
  */    
-class PacketMode(par: App) extends Mode(par, 2400.0)
+class PacketMode(par: App) extends Mode(par, 4800.0)
 {
     override val name = "packet"
     override val tooltip = "AX.25 and APRS"
@@ -480,12 +474,14 @@ class PacketMode(par: App) extends Mode(par, 2400.0)
         {
         val symbollen = samplesPerSymbol.toInt
         val halflen   = symbollen >> 1
+        //trace("symbollen: " + symbollen + "  halflen:" + halflen)
             
         val period = samplesSinceTransition % symbollen
+        //trace("period: " + period + "  halflen:" + halflen)
         if (period == halflen)
             {
-            octet = (octet >> 1) & 255
-            val bit = (inBit == lastBit)
+            octet = (octet >> 1) & 0xff
+            val bit = (inBit == lastBit) //nrzi
             lastBit = inBit
             if (bit) 
                 { ones += 1 ; octet |= 128 }
@@ -502,7 +498,7 @@ class PacketMode(par: App) extends Mode(par, 2400.0)
                         state    = RxTxd
                         bitcount = 0
                         }
-                 case RxTxd => 
+                case RxTxd => 
                     //trace("RxTxd")
                     bitcount += 1
                     if (bitcount >= 8)
@@ -553,7 +549,8 @@ class PacketMode(par: App) extends Mode(par, 2400.0)
                         }
                 case RxFlag2 =>
                     //we simply wanted that last bit
-                    processPacket(rxbuf, bufPtr)
+                    val outbuf = rxbuf.take(bufPtr)
+                    processPacket(outbuf)
                     for (i <- 0 until rxbuf.size)
                         rxbuf(i) = 0
                     state = RxStart                   
@@ -571,17 +568,18 @@ class PacketMode(par: App) extends Mode(par, 2400.0)
         new String(bytes)
         }        
     
-    def processPacket(data: Array[Int], len: Int) : Boolean =
+    def processPacket(data: Array[Int]) : Boolean =
         {
-        //println("raw:" + len)
+        val len = data.size
+        //trace("raw:" + len)
         if (len < 14)
             return true
-        val str = intToStr(data, 14, len-2)
+        //val str = intToStr(data, 14, len-2)
         //trace("txt: " + str)
         crc.reset
         for (i <- 0 until len)
-            crc.update(data(i))
-        val v = crc.value
+            crc.updateLE(data(i))
+        val v = crc.valueLE
         //trace("crc: %04x".format(v))
         if (v == 0xf0b8)
             {
